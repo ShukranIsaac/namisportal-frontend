9c63f0af6c16b1c2b7e2cba27b6226f402af18f8
paulse <paulse@student.matnat.uio.no>
2018-11-20 12:51:14 +0100

    added licensing page to demo from licennsing branch

--- src/components/licensing/index.js
+++ src/components/licensing/index.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import LicensingProfile from './LicensingProfileActivity';
+import LicensingSidebar from './LicensingSidebar';
 
 class Licensing extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      capacity: '',
+    }
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
   render(){
+    const { classes } = this.props;
+
     return (
-      <div>
-        Licensing
+      <div className={classes.root}>
+        <LicensingSidebar onChange={ this.handleChange } {...this.state}/>
+        <LicensingProfile onChange={ this.handleChange } {...this.state}/>
       </div>
     );
   }
 }
 
-export default Licensing;
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: `100%`,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+  }
+});
+
+Licensing.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Licensing);
