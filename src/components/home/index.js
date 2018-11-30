b88a5f1e72fe05e9ff90e84cfe7da73c866fd69f
paulse <paulse@student.matnat.uio.no>
2018-11-30 14:10:06 +0100

    edited homepage

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import { Elevation, Button, Card, Callout, Code, H5, Intent, Switch } from "@blueprintjs/core";
+import { Elevation, Button, Card, Callout, Code, Intent, Switch, } from "@blueprintjs/core";
 import { BrowserRouter as Route, Link } from "react-router-dom";
 import { Flex, Box } from 'reflexbox'
+
+import Directory from '../directory'
 import GIS from '../gis'
+import TASF from '../tasf'
+import Financing from '../financing'
+import Library from '../library'
+import Licensing from '../licensing'
 
 import './home.css'
 import Footer from '../footer';
@@ -23,8 +29,8 @@ class Home extends Component {
           w={1}
           className='landing-info'>
           <Box w={1/2} p={1} align='center'>
-          <Card interactive={true} elevation={Elevation.TWO}>
-            <h5 className="heading"><a href="#">Information for Mini-Grid Developers</a></h5>
+          <Card elevation={Elevation.TWO}>
+            <h4 className="heading"><a href="#">Information for Mini-Grid Developers</a></h4>
             <p>
               An information portal is a customized website that immerses information from a wide
               range of sources in a consistent and uniform manner. For this purpose, UNDP and
@@ -35,7 +41,6 @@ class Home extends Component {
               information; infrastructure; location of government public service institutions; location
               of other rural infrastructure, land use, environmental and social issues.
             </p>
-            <Button>Chek it out</Button>
           </Card>
           </Box>
         </Flex>
@@ -48,79 +53,96 @@ class Home extends Component {
           w={1}
           p={3}>
           <Box w={1/3} p={1}>
-          <Card interactive={true} elevation={Elevation.TWO}>
-            <h5 ><a className="heading" href="#">Licensing</a></h5>
+          <Card elevation={Elevation.TWO}>
+            <h4 ><a className="heading" href="#">Licensing</a></h4>
             <p>
             The Mini-Grids Information Portal's Licensing section provides 
             an overview of procedures, prerequisites and required documentation 
             for obtaining generation, distribution, and supply licenses for small 
             powerplants.
             </p>
-            <Button>Licensing Section</Button>
+            <Link to="licensing">
+              <Button intent="primary">Licensing Section</Button>
+            </Link>
           </Card>
           </Box>
           <Box w={1/3} p={1}>
-          <Card interactive={true} elevation={Elevation.TWO}>
-            <h5><a className="heading" href="#">Financing</a></h5>
+          <Card elevation={Elevation.TWO}>
+            <h4><a className="heading" href="#">Financing</a></h4>
             <p>
             Mini-Grid development in Tanzania may receive fincancing aid from REA. 
             The required documentation and an overview of the procedure is 
             presented in a dedicated relevant section of the Mini-Grids Information Portal.
             </p>
-            <Button>Financing Section</Button>
+            <Link to="financing">
+              <Button intent="primary">Financing Section</Button>
+            </Link>
           </Card>
           </Box>
           <Box w={1/3} p={1}>
-          <Card interactive={true} elevation={Elevation.TWO}>
-            <h5><a className="heading" href="#">Library</a></h5>
+          <Card elevation={Elevation.TWO}>
+            <h4><a className="heading" href="#">Library</a></h4>
             <p>
             The Library section of the Mini-Grids Information Portal caters to the need 
             of project developers to have all relevant legislature and relevant documents 
             collected at one place, easily accessible, and up-to-date.
 
             </p>
-            <Button>Library Section</Button>
+            <Link to="library">
+              <Button intent="primary">Library Section</Button>
+            </Link>
+            
           </Card>
           </Box>
           <Box w={1/3} p={1}>
-          <Card interactive={true} elevation={Elevation.TWO}>
-            <h5><a className="heading" href="#">GIS</a></h5>
+          <Card elevation={Elevation.TWO}>
+            <h4><a className="heading" href="#">GIS</a></h4>
             <p>
             To help mini-grid investors get an overview of facts about specific areas in 
             Tanzania, the GIS section presents geographically anchored information 
             (rural electrification, MV&HV lines, powerstations, etc.).
             </p>
             <Link to='/gis'>
-              <Button>GIS section</Button>
+              <Button intent="primary">GIS section</Button>
             </Link>
           </Card>
           </Box>
           <Box w={1/3} p={1}>
-          <Card interactive={true} elevation={Elevation.TWO}>
-            <h5><a className="heading" href="#">TASF</a></h5>
+          <Card elevation={Elevation.TWO}>
+            <h4><a className="heading" href="#">TASF</a></h4>
             <p>
             The Transactional Advisory Services Facility (TASF) provides experts to support 
             developers to strengthen mini-grids operating models, increase their commercial 
             viability and, ultimately, bankability.
             </p>
-            <Button>TASF section</Button>
+            <Link to="tasf">
+              <Button intent="primary">TASF section</Button>
+            </Link>
+            
           </Card>
           </Box>
           <Box w={1/3} p={1}>
-          <Card interactive={true} elevation={Elevation.TWO}>
-            <h5><a className="heading" href="#">Directory</a></h5>
+          <Card elevation={Elevation.TWO}>
+            <h4><a className="heading" href="#">Directory</a></h4>
             <p>
             The Portal allows the self-registration of state authorities, local authorities, 
             financing institutions and other entities of relevance to mini-grid development. 
             Their details are gathered in the Directory section.
             </p>
-            <Button>Directory section</Button>
+            <Link to="/directory">
+              <Button intent="primary">Directory section</Button>
+            </Link>
           </Card>
           </Box>
         </Flex>
       </div>
       <Footer/>
       <Route path="/gis" component={GIS} />
+      <Route path="/licensing" component={Licensing} />
+      <Route path="/financing" component={Financing} />
+      <Route path="/library" component={Library} />
+      <Route path="/directory" component={Directory} />
+      <Route path="/tasf" component={TASF} />
       </>
     );
   }
