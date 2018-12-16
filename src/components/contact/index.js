0d407a2314d027d4633bc2e7955631824e3cb7c0
isaacmw <imwakabira@ymail.com>
2018-12-16 14:05:13 +0100

    context api tutorial

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,12 +1,80 @@
 import React, { Component } from 'react';
 
-class Contact extends Component {
-  render(){
+// new context
+const AppContext = React.createContext();
+
+// then new provider
+class MyProvider extends Component {
+
+  state = {
+    name: 'Isaac',
+    age: 23,
+    cool: true,
+  }
+
+  render() {
+
+      return (
+        <AppContext.Provider value={{ state: this.state, }}>
+            {this.props.children}
+        </AppContext.Provider>
+      );
+
+  }
+
+}
+
+const Family = (props) => {
+
+  return (
+    <div className="family">
+      <Person />
+    </div>
+  );
+}
+
+class Person extends Component {
+
+  render() {
+
     return (
-      <div>
-        Contact
+      <div className="person">
+        <AppContext.Consumer>
+            {
+              (context) => {
+                  return (
+                    <React.Fragment>
+                        <p>Hey i am a {context.state.name}</p>
+                        <p>Hey i am a {context.state.age}</p>
+                    </React.Fragment>
+                  );
+              }
+            }
+        </AppContext.Consumer>
       </div>
     );
+  }
+}
+
+class Contact extends Component {
+
+  state = {
+    name: 'Isaac',
+    age: 23,
+    cool: true,
+  }
+
+  render(){
+
+    return (
+      <MyProvider>
+        <div>
+          Contact
+          <Family />
+        </div>
+      </MyProvider>
+    );
+
   }
 }
 
