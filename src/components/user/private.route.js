b2c972e6ac4a8ac772e070f317deedd868417aa6
isaacmw <imwakabira@ymail.com>
2019-01-03 13:14:07 +0100

    private route documentation

--- src/components/user/private.route.js
+++ src/components/user/private.route.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const UserPrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        localStorage.getItem('user')
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )} />
-);
+/**
+ * Checks if the user is logged in
+ * if yes show cms landing page else
+ * show loggin form.
+ * 
+ * @param component
+ * @param  props 
+ * @returns component to render
+ */
+const UserLoggedIn = ({component, props}) => {
+
+    return (
+        <Fragment>
+            
+            {
+                
+                localStorage.getItem('user') ? <component {...props} /> : 
+                <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+            }
+
+        </Fragment>
+    );
+
+};
+
+/**
+ * Directs the user to the right route depending 
+ * on the user credentials if any provided else loggin form.
+ *  
+ * @param component
+ * @param rest
+ * @returns component
+ */
+const UserPrivateRoute = ({ component: Component, ...rest }) => {
+
+    return (
+        <Fragment>
+
+            <Route {...rest} render={props => (<UserLoggedIn props={props} component={Component }/>)}/>
+
+        </Fragment>
+    );
+};
 
 export default UserPrivateRoute;
