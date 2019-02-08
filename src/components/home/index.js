768f2de2c5b3e511835c8c8e1b829effa4e2f2be
isaacmw <imwakabira@ymail.com>
2019-02-08 12:00:01 +0100

    home loader

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -26,7 +26,7 @@ class Home extends Component {
     const { home } = this.props;
 
     if (home.subCategories === null || home.subCategories === undefined) {
-      return <div />
+      return <div className="loader"/>
     }
 
     const main_section = filterSection(home.subCategories, "Information for Mini-Grid Developers");
