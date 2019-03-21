4eba04e8ee01156b9b1b520313629bba9d946556
Shukurani Isaac <imwakabira@ymail.com>
2019-03-21 11:36:03 +0100

    cms create new library stakeholder input fields

--- src/components/library/library.create.document.js
+++ src/components/library/library.create.document.js
@@ -13,6 +13,7 @@ import { Intent, Button } from '@blueprintjs/core';
 import styles from '../contact/form.styles';
 import { MuiFormFileinputField } from '../forms/form.fileinput.field';
 import { UserProfile } from '../user/user.profile';
+import { FormTextInputField } from '../forms/form.textinput.field';
 // import FormSelectMultiple from '../forms/form.multiple.options.field';
 
 /**
@@ -131,39 +132,20 @@ class CreateLibraryItem extends Component {
 
                     <Divider />
 
-                    <Field
+                    <FormTextInputField 
+                        classes={ classes }
                         name='name'
-                        component={ input => {
-                            return (
-                                <RenderBootstrapField
-                                    classes={ classes }
-                                    label='Name'
-                                    defaultValue="Edit document name..."
-                                    name="name"
-                                    type="text"
-                                    props={ input }
-                                />
-                            );
-                        }}
+                        label="Name"
+                        placeholder="Edit document name..."
+                        type="text"
                     />
 
-                    <Field
-                        name='summary'
-                        component={ input => {
-
-                            return (
-                                <RenderBootstrapField
-                                    classes={ classes }
-                                    label='Summary'
-                                    defaultValue="Edit document summary..."
-                                    // value={ resource_plan.summary }
-                                    name="summary"
-                                    type="text"
-                                    props={ input }
-                                />
-                            );
-
-                        }}
+                    <FormTextInputField 
+                        classes={ classes }
+                        name='summary' 
+                        label="Summary"
+                        placeholder="Edit document summary..."
+                        type="text"
                         multiline={true}
                         rows="10"
                     />
@@ -183,31 +165,9 @@ class CreateLibraryItem extends Component {
                     <div className={ classes.margin } />
                     <div className={ classes.margin } />
 
-                    {/* <ButtonControl 
-                        intent={Intent.PRIMARY} 
-                        value="Save"
-                        name="save"
-                    /> */}
                     <Button type="submit" disabled={!valid || pristine || submitting} intent="success" text="Save" />
                     
                     <Button className={ classes.margin } name="default" intent="primary" text="Cancel" onClick={ e => handleClick(e) } /> 
-                
-
-                    {/* <ButtonControl 
-                        intent={Intent.SUCCESS} 
-                        value="Publish" 
-                        name="publish"
-                        handleClick={e => handleClick(e) } 
-                    /> */}
-
-                    {/* <ButtonControl 
-                        intent={Intent.WARNING} 
-                        value="Unpublish" 
-                        name="unpublish"
-                        handleClick={e => handleClick(e) } 
-                    /> */}
-
-                    {/* <ButtonControl intent={Intent.DANGER} value="Archive" name="archive" handleClick={e => handleClick(e) } /> */}
                 
                 </form>
 
