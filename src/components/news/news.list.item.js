ec8ef95d8f800ee90e91c8c9e45b7070d9a0458b
ShukuranIsaac <imwakabira@ymail.com>
2019-09-13 14:24:08 +0200

    format article date

--- src/components/news/news.list.item.js
+++ src/components/news/news.list.item.js
@@ -23,7 +23,7 @@ const NewsListItem = ({ when, splitCount, classes, article })=> {
                                 article: {
                                     title: article.title,
                                     text: article.article,
-                                    createdDate: article.createdDate
+                                    createdDate: new Date(article.createdDate).toLocaleDateString()
                                 }
                             }
                         }}
@@ -34,7 +34,7 @@ const NewsListItem = ({ when, splitCount, classes, article })=> {
                 <p variant="caption">
                     <i> 
                         {/* By John Doe (<a href="mailto:newseditor@grid.mw" className={classes.link}>newseditor@grid.mw</a>), */}
-                        <span className={classes.when}>{ article.createdDate }</span>
+                        <span className={classes.when}>{ new Date(article.createdDate).toLocaleDateString() }</span>
                     </i>
                 </p>
             </div>
@@ -47,7 +47,7 @@ const NewsListItem = ({ when, splitCount, classes, article })=> {
                         article: {
                             title: article.title,
                             text: article.article,
-                            createdDate: article.createdDate
+                            createdDate: new Date(article.createdDate).toLocaleDateString()
                         },
                     }
                 }}
