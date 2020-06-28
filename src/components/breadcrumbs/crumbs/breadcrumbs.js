7ea3ce8f451207ea1370c32cf001140e4d91fa9c
ShukranIsaac <imwakabira@ymail.com>
2020-06-28 16:02:47 +0200

    Breadcrumbs: code refactor

--- src/components/breadcrumbs/crumbs/breadcrumbs.js
+++ src/components/breadcrumbs/crumbs/breadcrumbs.js
@@ -1,112 +1,103 @@
 // Import External Dependencies
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
-// TODO: Use imitation and allow it to be passed as a prop
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 // Specify BEM block name
-const block = 'breadcrumbs'
+const block = 'breadcrumbs';
 
 // Create and export the component
-export class Breadcrumbs extends React.Component {
-    static propTypes = {
-        className: PropTypes.string,
-        hidden: PropTypes.bool,
-        separator: PropTypes.node,
-        setCrumbs: PropTypes.func,
-        wrapper: PropTypes.oneOfType([
-            PropTypes.func,
-            PropTypes.instanceOf(
-                React.Component
-            )
-        ]),
-        children: PropTypes.oneOfType([
-            PropTypes.node,
-            PropTypes.arrayOf(
-                PropTypes.node
-            )
-        ])
-    }
+export const Breadcrumbs = ({
+    className, 
+    hidden, 
+    wrapper: Wrapper, 
+    setCrumbs, 
+    breadcrumbs,
+    ...props 
+}) => {
+    const hiddenMod = hidden ? `${block}--hidden` : '';
 
-    static defaultProps = {
-        className: '',
-        hidden: false,
-        separator: '>',
-        setCrumbs: undefined,
-        wrapper: props => (
-            <nav {...props}>
-                {props.children}
-            </nav>
-        )
-    }
+    let crumbs = breadcrumbs.sort((a, b) => {
+        return a.pathname.length - b.pathname.length
+    })
 
-    render() {
-        let { 
-            className, hidden, 
-            wrapper: Wrapper, 
-            setCrumbs, breadcrumbs 
-        } = this.props;
-        
-        let hiddenMod = hidden ? `${block}--hidden` : '';
+    if (setCrumbs) crumbs = setCrumbs(crumbs);
 
-        let crumbs = breadcrumbs.sort((a, b) => {
-            return a.pathname.length - b.pathname.length
-        })
+    return (
+        <div className={className}>
+            <Wrapper 
+                className={`${block} ${hiddenMod}`} 
+                style={{ backgroundColor: "#416fd1" }}>
+                <div className={`${block}__inner`}>
+                    {
+                        crumbs.map((crumb, i) => (
+                            <span key={crumb.id} className={`${block}__section`}>
+                                <Link
+                                    exact
+                                    style={{ color: "#fff" }}
+                                    className={`${block}__crumb`}
+                                    activeClassName={`${block}__crumb--active`}
+                                    to={{
+                                        pathname: crumb.pathname,
+                                        search: crumb.search,
+                                        state: crumb.state
+                                    }}>
+                                    {crumb.title}
+                                </Link>
 
-        if (setCrumbs) {
-            crumbs = setCrumbs(crumbs)
-        }
+                                {
+                                    i < crumbs.length - 1 ? (
+                                        <span className={`${block}__separator`}>
+                                            {props.separator}
+                                        </span>
+                                    ) : <span />
+                                }
+                            </span>
+                        ))
+                    }
+                </div>
+            </Wrapper>
 
-        return (
-            <div className={className}>
-                <Wrapper 
-                    className={`${block} ${hiddenMod}`} 
-                    style={{ backgroundColor: "#416fd1" }}>
-                    <div className={`${block}__inner`}>
-                        {
-                            crumbs.map((crumb, i) => (
-                                <span key={crumb.id} className={`${block}__section`}>
-                                    <Link
-                                        exact
-                                        style={{ color: "#fff" }}
-                                        className={`${block}__crumb`}
-                                        activeClassName={`${block}__crumb--active`}
-                                        to={{
-                                            pathname: crumb.pathname,
-                                            search: crumb.search,
-                                            state: crumb.state
-                                        }}>
-                                        {crumb.title}
-                                    </Link>
+            {props.children}
 
-                                    {
-                                        i < crumbs.length - 1 ? (
-                                            <span className={`${block}__separator`}>
-                                                {this.props.separator}
-                                            </span>
-                                        ) : <span />
-                                    }
-                                </span>
-                            ))
-                        }
-                    </div>
-                </Wrapper>
-
-                {this.props.children}
-
-            </div>
-        )
-    }
+        </div>
+    )
 }
 
-const mapStateToProps = (state) => {
-
-    return {
-        breadcrumbs: state.breadcrumbs,
-    };
-
+Breadcrumbs.propTypes = {
+    className: PropTypes.string,
+    hidden: PropTypes.bool,
+    separator: PropTypes.node,
+    setCrumbs: PropTypes.func,
+    wrapper: PropTypes.oneOfType([
+        PropTypes.func,
+        PropTypes.instanceOf(
+            React.Component
+        )
+    ]),
+    children: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.arrayOf(
+            PropTypes.node
+        )
+    ])
 }
 
-export default connect(mapStateToProps, null)(Breadcrumbs);
+Breadcrumbs.defaultProps = {
+    className: '',
+    hidden: false,
+    separator: '>',
+    wrapper: props => (
+        <nav {...props}>
+            {props.children}
+        </nav>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    breadcrumbs: state.breadcrumbs,
+});
+
+export default connect(mapStateToProps, 
+    null)(Breadcrumbs);
