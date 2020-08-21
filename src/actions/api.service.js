343e7b53e5ba7bd8ff7a8d2d74a77f2d6a2de7fb
ShukranIsaac <imwakabira@ymail.com>
2020-08-21 10:43:40 +0200

    components: url

--- src/actions/api.service.js
+++ src/actions/api.service.js
@@ -23,7 +23,7 @@ const progressEvent = (dispatch, config) => ({
  */
 export const get = async (dispatch, url) => {
 
-    return await Config.DEV_REMOTE_API_URL.get(url)
+    return await Config.PROD_REMOTE_API_URL.get(url)
 
         .then((response) => {
 
@@ -52,7 +52,7 @@ export const emailMessage = async (dispatch, url, data) => {
     const config = new Headers();
     config.append('Access-Control-Allow-Origin', Config.ACCESS_ALLOW_ORIGIN);
 
-    return await Config.DEV_REMOTE_API_URL.post(url, data, config)
+    return await Config.PROD_REMOTE_API_URL.post(url, data, config)
 
         .then(response => {
 
@@ -82,7 +82,7 @@ export const post = async (dispatch, url, data) => {
     const config = new Headers();
     config.append('Access-Control-Allow-Origin', Config.ACCESS_ALLOW_ORIGIN);
 
-    return await Config.DEV_REMOTE_API_URL.post(url, data, config)
+    return await Config.PROD_REMOTE_API_URL.post(url, data, config)
 
         .then(response => {
 
@@ -122,7 +122,7 @@ export const upload = async (dispatch, url, data) => {
         form.append('file', data.image[0]);
     }
 
-    return await Config.DEV_REMOTE_API_URL
+    return await Config.PROD_REMOTE_API_URL
 
         .post(url, form, progressEvent(dispatch, null))
 
@@ -169,7 +169,7 @@ export const update = async (dispatch, url, data) => {
         form.append('description', data.description)
     }
 
-    return await Config.DEV_REMOTE_API_URL
+    return await Config.PROD_REMOTE_API_URL
 
         .patch(url, (data.file !== undefined && data.file !== null) 
             ? form : data, progressEvent(dispatch, null))
@@ -206,7 +206,7 @@ export const put = async (dispatch, url, data) => {
     config.append('Access-Control-Allow-Origin', Config.ACCESS_ALLOW_ORIGIN);
     // config.append('withCredentials', true);
 
-    return await Config.DEV_REMOTE_API_URL.put(url, data, config)
+    return await Config.PROD_REMOTE_API_URL.put(url, data, config)
 
         .then(response => {
 
@@ -238,7 +238,7 @@ export const patch = async (dispatch, url, data) => {
     config.append('Access-Control-Allow-Origin', Config.ACCESS_ALLOW_ORIGIN);
     // config.append('withCredentials', true);
 
-    return await Config.DEV_REMOTE_API_URL.patch(url, data, progressEvent(dispatch))
+    return await Config.PROD_REMOTE_API_URL.patch(url, data, progressEvent(dispatch))
 
         .then(response => {
 
@@ -263,7 +263,7 @@ export const patch = async (dispatch, url, data) => {
  */
 export const _delete = async (dispatch, url) => {
 
-    return await Config.DEV_REMOTE_API_URL.delete(url)
+    return await Config.PROD_REMOTE_API_URL.delete(url)
 
         .then(response => {
 
